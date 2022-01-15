51cfe64ab859f104db38d54a1f94600700ec170c
stoffel-brian <90878973+stoffel-brian@users.noreply.github.com>
2022-01-15 17:03:34 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,16 @@ library(dplyr)
 lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=MechaCar_mpg)
 
 summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=MechaCar_mpg))
+
+
+df = Suspension_Coil
+
+print(df)
+
+total_summary %>%  group_by(Manufacturing_Lot) %>% summarise(mean(df$PSI),median(df$PSI),sd(df$PSI), var(df$PSI))
+
+t.test(df$PSI)
+
+t.test(df$PSI,subset = Manufacturing_Lot %in% c(1))
+t.test(df$PSI,subset = Manufacturing_Lot %in% c(2))
+t.test(df$PSI,subset = Manufacturing_Lot %in% c(3))
